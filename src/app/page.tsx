b5bb79beccf9a9d32d5699c9698bc1c5a678f0c4
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-03-24 00:40:26 -0300

    feat: :sparkles: Geolocation Permission

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,33 @@
-import { redirect } from 'next/navigation'
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { Loader } from 'lucide-react'
 
 export default function Home() {
-  // Redireciona para a primeira cidade
-  redirect(`/-7.6117323/15.0563515}`)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords
+          router.push(`/${latitude}/${longitude}`)
+        },
+        () => {
+          // Caso o usuário negue a permissão
+          router.push(`/-7.6117323/15.0563515`)
+        },
+      )
+    } else {
+      // Caso o navegador não suporte geolocalização
+      router.push(`/-7.6117323/15.0563515`)
+    }
+  }, [])
+
+  return (
+    <div className='flex flex-col justify-center items-center min-h-screen bg-gradient-to-br from-blue-700 to-blue-400 text-white font-[family-name:var(--font-geist-sans)] p-4'>
+      <Loader className='animate-spin' size={30} />
+    </div>
+  )
 }
